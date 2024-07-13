171abc93cbbd93f97b499522d25b1ea06ca47eaa
Itssagarcode <sagarmaithil70@gmail.com>
2024-07-13 23:08:43 +0530

    day1

---
+++ day1_variables_dataType.js
@@ -0,0 +1,49 @@
+var rollno = 2342135 ;
+console.log(rollno)
+
+
+//task2 
+
+
+let name = "Bhushan";
+console.log(name);
+
+//task3
+
+const active = true;
+console.log(active);
+
+// task4
+
+let number = 222222222;
+console.log(typeof(number))
+
+let fullName = "Bhushan"
+console.log(typeof(fullName));
+
+let loggedIn = true;
+console.log(typeof(loggedIn));
+
+const person = {
+    name : "bhushan",
+    class : "MCA",
+    rollNO : 2342135
+}
+console.log(typeof(person))
+
+const colour = ["Red", "Blue", "Yellow", "Orange"]
+console.log(typeof(colour))
+
+// task5
+
+let profile = "frontend developer"
+
+profile = "backend developer"
+console.log(profile);
+
+// task6
+
+const rent  = 3000;
+rent = 3200;
+console.log(rent);
+
